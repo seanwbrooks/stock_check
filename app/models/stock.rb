171625517eccdea2bfbf5eb60c64db1d3e0d5219
Sean <swbrooks87@gmail.com>
2017-07-24 08:09:30 -0400

    Added uniqueness to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-  validates :symbol, presence: true
+  validates :symbol, presence: true, uniqueness: true
 
   def buy?(stock)
     buy = (stock.peg_ratio.to_f * (0.7)) + ((stock.ask.to_f - stock.fiftyday_moving_average.to_f) * (0.3))
